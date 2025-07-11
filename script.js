c8005c76e35778f759f994b403e12193479908a4
Moteen Raza <moteenraza@gmail.com>
2025-07-11 15:13:58 -0400

    implement form validation

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ const prevButton = document.querySelectorAll(".prevButton"); // create a variabl
 const projects = document.querySelectorAll(".projectGallery"); // create a variable for projectGlarrey class
 let currentIndex = 0; // create a variable for current index value
 
+// create variables for contact form
+const form = document.getElementById("registerForm");
+const nameInput = document.getElementById("name");
+const emailInput = document.getElementById("emailAddress");
+
+// error message
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+
+// success message
+
+const successMessage = document.getElementById("successMessage");
+
 // create a function that check current index and check if its beind displayed as flex or none.
 function updateProject() {
   projects.forEach((project, index) => {
@@ -60,3 +73,38 @@ prevButton.forEach((btn) => {
     updateProject();
   });
 });
+
+// create a submit form function that checks for name and email validation
+form.addEventListener("submit", function (event) {
+  nameError.textContent = "";
+  emailError.textContent = "";
+  successMessage.textContent = "";
+
+  let isValid = true;
+
+  if (nameInput.value.trim() === "") {
+    nameError.textContent = "Please enter your name";
+    event.preventDefault();
+    isValid = false;
+  }
+
+  if (emailInput.value.trim() === "") {
+    emailError.textContent = "Please enter your email";
+    event.preventDefault();
+    isValid = false;
+  } else if (!validateEmail(emailInput.value)) {
+    emailError.textContent = "Please enter a valid email address";
+    isValid = false;
+  }
+  if (!isValid) {
+    event.preventDefault();
+  } else {
+    successMessage.textContent = "Form submitted";
+  }
+});
+
+// create a function that test email structure
+function validateEmail(email) {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return re.test(String(email));
+}
